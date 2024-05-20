9ebb69be6cf637ef8ab8dfbb9d6fa0aa41267bb4
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-05-20 15:10:39 +0300

    update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,16 +8,16 @@ import request from 'request';
  * @param {string} url - The URL to fetch data from.
  * @returns {Promise<Object>} - A promise that resolves with the parsed JSON data.
  */
-function get(url) {
-    return new Promise((resolve, reject) => {
-        request.get(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(JSON.parse(body));
-            }
-        });
+function get (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
+  });
 }
 
 /**
@@ -25,26 +25,26 @@ function get(url) {
  *
  * @param {string} movieID - The ID of the movie to fetch characters from.
  */
-async function printCharacters(movieID) {
-    try {
-        const apiFilmLink = `https://swapi-api.alx-tools.com/api/films/`;
-        const filmData = await get(apiFilmLink + movieID);
-        const characters = filmData.characters;
+async function printCharacters (movieID) {
+  try {
+    const apiFilmLink = 'https://swapi-api.alx-tools.com/api/films/';
+    const filmData = await get(apiFilmLink + movieID);
+    const characters = filmData.characters;
 
-        for (const characterUrl of characters) {
-            const characterData = await get(characterUrl);
-            console.log(characterData.name);
-        }
-    } catch (error) {
-        console.error('Error occurred:', error);
-        process.exit(1);
+    for (const characterUrl of characters) {
+      const characterData = await get(characterUrl);
+      console.log(characterData.name);
     }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    process.exit(1);
+  }
 }
 
 // Ensure correct usage
 if (process.argv.length !== 3) {
-    console.log(`Usage: ${process.argv[1]} movieID`);
-    process.exit(1);
+  console.log(`Usage: ${process.argv[1]} movieID`);
+  process.exit(1);
 }
 
 // Extract movieID from command-line arguments
