1fb1366b77921d1006a2fd706f917c05b98e51ed
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-05-24 10:46:36 +0300

    build(deps): update 0-starwars_characters.js, package-lock.json and package.json

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,54 @@
 #!/usr/bin/node
 
-const request = require('request');
-const filmId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+import request from 'request';
 
-request(url, async (err, response, body) => {
-    if (err) {
-        console.log(err);
-    }
-    for (const characterId of JSON.parse(body).characters) {
-        await new Promise((resolve, reject) => {
-            request(characterId, (err, response, body) => {
-                if (err) {
-                    reject(err);
-                }
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
+/**
+ * Fetches data from a given URL using the request module and returns a promise.
+ *
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Promise<Object>} - A promise that resolves with the parsed JSON data.
+ */
+function get(url) {
+    return new Promise((resolve, reject) => {
+        request.get(url, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(JSON.parse(body));
+            }
         });
+    });
+}
+
+/**
+ * Fetches and prints the names of characters in a Star Wars movie specified by movieID.
+ *
+ * @param {string} movieID - The ID of the movie to fetch characters from.
+ */
+async function printCharacters(movieID) {
+    try {
+        const apiFilmLink = `https://swapi-api.alx-tools.com/api/films/`;
+        const filmData = await get(apiFilmLink + movieID);
+        const characters = filmData.characters;
+
+        for (const characterUrl of characters) {
+            const characterData = await get(characterUrl);
+            console.log(characterData.name);
+        }
+    } catch (error) {
+        console.error('Error occurred:', error);
+        process.exit(1);
     }
-});
+}
+
+// Ensure correct usage
+if (process.argv.length !== 3) {
+    console.log(`Usage: ${process.argv[1]} movieID`);
+    process.exit(1);
+}
+
+// Extract movieID from command-line arguments
+const movieID = process.argv[2];
+
+// Execute the main function
+printCharacters(movieID);
