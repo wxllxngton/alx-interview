ec79dcc7d126664437523ba5e60383d0801a1766
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-06-06 20:35:11 +0300

    update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,44 @@
 
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
+/**
+ * Recursively prints character names from an array of URLs.
+ *
+ * @param {string[]} urls - Array of character URLs.
+ * @param {number} index - Current index in the array.
+ */
+const printCharacterNames = (urls, index) => {
+  if (index === urls.length) return;
+  request(urls[index], (err, response, body) => {
     if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
+      console.error(err);
+      return;
     }
+    console.log(JSON.parse(body).name);
+    printCharacterNames(urls, index + 1);
   });
 };
 
-request(
-    `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-    (err, response, body) => {
-      if (err) {
-        throw err;
-      } else {
-        const chars = JSON.parse(body).characters;
-        req(chars, 0);
-      }
-    }
-);
+// Retrieve the Movie ID from the command line arguments
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Please provide a Movie ID');
+  process.exit(1);
+}
+
+// Construct the API URL for the specified movie
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+/**
+ * Sends a request to the Star Wars API to get the characters of the specified movie.
+ *
+ * @param {string} apiUrl - URL to the Star Wars API film endpoint.
+ */
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const characters = JSON.parse(body).characters;
+  printCharacterNames(characters, 0);
+});
