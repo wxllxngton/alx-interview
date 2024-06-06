fffac834d3b3882c84f8e0a2179e0e5331440ad3
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-06-06 15:59:44 +0300

    update 1 file and copy 1 file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,54 +1,27 @@
 #!/usr/bin/node
 
-import request from 'request';
+const request = require('request');
 
-/**
- * Fetches data from a given URL using the request module and returns a promise.
- *
- * @param {string} url - The URL to fetch data from.
- * @returns {Promise<Object>} - A promise that resolves with the parsed JSON data.
- */
-function get (url) {
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body));
-      }
+const req = (arr, i) => {
+    if (i === arr.length) return;
+    request(arr[i], (err, response, body) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(JSON.parse(body).name);
+            req(arr, i + 1);
+        }
     });
-  });
-}
+};
 
-/**
- * Fetches and prints the names of characters in a Star Wars movie specified by movieID.
- *
- * @param {string} movieID - The ID of the movie to fetch characters from.
- */
-async function printCharacters (movieID) {
-  try {
-    const apiFilmLink = 'https://swapi-api.alx-tools.com/api/films/';
-    const filmData = await get(apiFilmLink + movieID);
-    const characters = filmData.characters;
-
-    for (const characterUrl of characters) {
-      const characterData = await get(characterUrl);
-      console.log(characterData.name);
+request(
+    `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+    (err, response, body) => {
+        if (err) {
+            throw err;
+        } else {
+            const chars = JSON.parse(body).characters;
+            req(chars, 0);
+        }
     }
-  } catch (error) {
-    console.error('Error occurred:', error);
-    process.exit(1);
-  }
-}
-
-// Ensure correct usage
-if (process.argv.length !== 3) {
-  console.log(`Usage: ${process.argv[1]} movieID`);
-  process.exit(1);
-}
-
-// Extract movieID from command-line arguments
-const movieID = process.argv[2];
-
-// Execute the main function
-printCharacters(movieID);
+);
